640cf40d5cab702f740447e3c6e1758f503ac57a
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-12 20:42:36 +0530

    [Anurag] Add: Find Max Float

--- FindMaximumNumberTest/UnitTest1.cs
+++ FindMaximumNumberTest/UnitTest1.cs
@@ -30,5 +30,30 @@ namespace FindMaximumNumberTest
             int num = max.FindMaxIntNumber(2, 1, 3);
             Assert.AreEqual(num, 3);
         }
+
+        [Test]
+        public void GivenMaxFloatFirstNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(3.3f,2.2f,1.1f);
+            Assert.AreEqual(num, 3.3f);
+        }
+
+        [Test]
+        public void GivenMaxFloatSecondNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(2.2f, 3.3f, 1.1f);
+            Assert.AreEqual(num, 3.3f);
+        }
+
+        [Test]
+        public void GivenMaxFloatThirdtNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(1.1f, 2.2f, 3.3f);
+            Assert.AreEqual(num, 3.3f);
+        }
+
     }
 }
