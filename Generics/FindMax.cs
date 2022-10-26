c7fbbe1cc0511299f42813c8daaa81a265baa4d3
mendhe1020 <anurag.mendhe14@gmail.com>
2022-10-26 15:45:54 +0530

    [Anurag] Add: Find Max

---
+++ Generics/FindMax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class FindMax<T> where T:IComparable 
+    {
+        public T first, second, third;
+        public FindMax(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public T FindMaxmum()
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                return first;
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+                return second;
+            else
+                return third;
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,10 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
-           
-            
+            Console.WriteLine("Enter the First number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Enter the second number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number: ");
+            int third = Convert.ToInt32(Console.ReadLine());
+
+            FindMax<int> obj = new FindMax<int>(first, second, third);
+            int Max=obj.FindMaxmum();
+
+            Console.WriteLine("Max Num:" + Max);
         }
     }
 }
